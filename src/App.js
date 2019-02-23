058deeaee2afce0767418b145eddea780e41b3e7
shaojunda <shaojunda@gmail.com>
2019-02-23 20:51:41 +0800

    ADD: react with redux demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { SIGHUP } from 'constants';
 class App extends Component {
+  add1() {
+    this.props.onAdd1()
+  }
+
+  minus1() {
+    this.props.onMinus1()
+  }
+
+  add1IfOdd() {
+    this.props.onAdd1IfOdd()
+  }
+
+  add1After2Sec() {
+    this.props.onAdd1After2Sec()
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        你点击了 <span id="value">{this.props.value}</span> 次
+        <div>
+          <button id="add1" onClick={this.add1.bind(this)} >+1</button>
+          <button id="minus1" onClick={this.minus1.bind(this)}>-1</button>
+          <button id="add1IfOdd" onClick={this.add1IfOdd.bind(this)}>如果是单数就 + 1</button>
+          <button id="add1After2Sec" onClick={() => {this.add1After2Sec()}}>两秒钟后 + 1</button>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducers = (state, action) => {
+  if (state === undefined) {
+    return 0
+  } else {
+    let newState
+    switch (action.type) {
+      case 'add':
+      newState = state + action.payload
+        return newState
+      case 'minus':
+        newState = state - action.payload
+        return newState
+      default:
+        return 0
+    }
+  }
+}
+var store = createStore(reducers)
+render()
+store.subscribe(render)
+
+function onAdd1IfOdd() {
+  if(store.getState() % 2 === 1) {
+    store.dispatch({type: 'add', payload: 1})
+  }
+}
+
+function onAdd1After2Sec() {
+  setTimeout(() => {
+    store.dispatch({type: 'add', payload: 1})
+  }, 2000)
+}
+
+function render() {
+  ReactDOM.render(<App value={store.getState()}
+    onAdd1={() => store.dispatch({type: 'add', payload: 1})}
+    onMinus1={() => store.dispatch({type: 'minus', payload: 1})}
+    onAdd1IfOdd={onAdd1IfOdd}
+    onAdd1After2Sec={onAdd1After2Sec}
+    />,
+    document.getElementById('root')
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
