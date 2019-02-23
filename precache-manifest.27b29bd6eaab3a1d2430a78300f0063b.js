d0f25be6df2d67b9ccd0e4f4941bab2b68e6a262
shaojunda <shaojunda@gmail.com>
2019-02-23 22:07:11 +0800

    Updates

---
+++ precache-manifest.27b29bd6eaab3a1d2430a78300f0063b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c586d52371981d2a3e97",
+    "url": "/demo-react-redux-1/static/js/runtime~main.c586d523.js"
+  },
+  {
+    "revision": "505e04988eab19c71df2",
+    "url": "/demo-react-redux-1/static/js/main.505e0498.chunk.js"
+  },
+  {
+    "revision": "dc3175dcd857d3740839",
+    "url": "/demo-react-redux-1/static/js/2.dc3175dc.chunk.js"
+  },
+  {
+    "revision": "505e04988eab19c71df2",
+    "url": "/demo-react-redux-1/static/css/main.7725bcf0.chunk.css"
+  },
+  {
+    "revision": "c6b9d3555076d59beecb2f806dd3aeca",
+    "url": "/demo-react-redux-1/index.html"
+  }
+];
